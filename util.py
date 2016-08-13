ead8fd8a9a17e1993673f2cc125c1776b29c866c
Vaibhav Maheshwai <vaibhav.dkm@gmail.com>
2016-08-13 19:32:04 +0530

    completed easy operations on number

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def easy_addition
+def easy_addition():
 	a = randint(1,20)
 	b = randint(1,20)
 	question = 'Add the numbers' + str(a) + ' and ' + str(b)
@@ -12,16 +12,46 @@ def easy_addition
 	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) } 
 	return dict 
 
-def easy_subtraction
+def easy_subtraction():
+	a = randint(1,20)
+	b = randint(1,20)
+	question = 'Add the numbers' + str(a) + ' and ' + str(b)
+	answer = a - b 
+	option1 = answer - randint(1,3) 
+	option2 = answer + randint(3,5) 
+	option3 = answer - randint(5,7) 
+	option4 = answer + randint(8,11)
+	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) } 
+	return dict 
+
+def easy_multiply(): 
+	a = randint(1,20)
+	b = randint(1,20)
+	question = 'Add the numbers' + str(a) + ' and ' + str(b)
+	answer = a * b 
+	option1 = answer - randint(1,3) 
+	option2 = answer + randint(3,5) 
+	option3 = answer - randint(5,7) 
+	option4 = answer + randint(8,11)
+	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) } 
+	return dict 
+
+def easy_divide(): 
+	a = randint(1,20)
+	b = randint(40,60)
+	question = 'Add the numbers' + str(a) + ' and ' + str(b)
+	while(b%a == 0):
+		answer = b/a
+		a = a + 1
+	option1 = answer - randint(1,3) 
+	option2 = answer + randint(3,5) 
+	option3 = answer - randint(5,7) 
+	option4 = answer + randint(8,11)
+	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) } 
+	return dict 
 
 
-def easy_multiply 
-
-
-def easy_divide 
-
-
-def medium_operation
+def medium_operation():
 
 
 def advacned_operation
