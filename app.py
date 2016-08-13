c70306955ca07d0bc1f8a57eed27c62c9e2db088
smit thakkar <smit@smits-MacBook-Air.local>
2016-08-13 23:15:06 +0530

    ahh

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ import traceback
 import random
 from constants import *
 import os
-from util import send_text_message
-from util import messaging_events
+from util import *
 
 app = Flask(__name__)
 
-PAT = 'EAANHAD7qAxkBAH4ZAZBdmXZAqAC3kYK5YjKngMo1HPVKMIp95dLl6fwBwvptwHg9WQFNWBYcVkaW1k8AiFAZA4vM0rzwlTBdIp5y4R95aZByoemeBPJDrOKXryq0RLNlzZBv6ZCMwuZB9YXsP2w2ghuZB70PDsPg7oYGbYOZB3sAdwDAZDZD'
+
 
 @app.route('/webhook', methods = ['GET'])
 def webhook():
@@ -22,15 +21,22 @@ def webhook():
 
 @app.route('/webhook', methods = ['POST'])
 def handle_message():
-    print "Handling Messages"
-    payload = request.get_data()
-    print payload
-    for sender, message in messaging_events(payload):
-        print "Incoming from %s: %s" % (sender, message)
-        send_text_message(PAT, sender, message)
-    return "ok"
+    try:
+        payload = request.get_data()
+        sender, message = messaging_events(payload)
+        if message == "click":
+            send_typing_status(sender)
+            send_button_template_message(sender, "Choose from the below",[
+                generate_button("Hello", "HelloButton"),
+                generate_button("How are you ?", "HelloButton")
+            ])
+        else:
+            send_text_message(sender, message)
+        return "ok"
+    except:
+        print "message with shit"
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
-    app.run(host = "0.0.0.0",port = port)
+    app.run(host = "0.0.0.0",port = port, debug = True)
--- util.py
+++ util.py
@@ -1,28 +1,77 @@
 import json
 import requests
+from constants import SEND_MESSAGE_URL, PAT as token
 
-def send_text_message(token, recipient, text):
+def send_typing_status(recipient):
   """Send the message text to recipient with id recipient.
   """
-
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+  r = requests.post(SEND_MESSAGE_URL,
     params={"access_token": token},
     data=json.dumps({
       "recipient": {"id": recipient},
-      "message": {"text": text.decode('unicode_escape')}
+      "sender_action": "typing_on"
     }),
     headers={'Content-type': 'application/json'})
   if r.status_code != requests.codes.ok:
     print r.text
 
+
+def send_text_message(recipient, text):
+  """Send the message text to recipient with id recipient.
+  """
+
+  r = requests.post(SEND_MESSAGE_URL,
+    params={"access_token": token},
+    data=json.dumps({
+      "recipient": {"id": recipient},
+      "message": {"text": text.decode('unicode_escape')},
+    }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
+
+def send_button_template_message(recipient, text, buttons):
+    r = requests.post(SEND_MESSAGE_URL,
+          params = {'access_token' : token},
+          data = json.dumps({
+              "recipient" : {"id" : recipient},
+              "message" : {
+                  "attachment" : {
+                  "type" : "template",
+                  "payload" : {
+                      "template_type" : "button",
+                      "text" : text,
+                      "buttons" : buttons
+                  }
+              }}
+          }),
+          headers={'Content-type': 'application/json'})
+    print r.text
+
+
+def generate_button(text, payload = None, type="text", url = None):
+    if type == "url":
+        return {
+            "type" : "web_url",
+            "url" : text,
+            "title" : text
+        }
+    else:
+        return {
+            "type" : "postback",
+            "title" : text,
+            "payload" : payload
+        }
+
 def messaging_events(payload):
   """Generate tuples of (sender_id, message_text) from the
   provided payload.
   """
+  import pdb; pdb.set_trace()
   data = json.loads(payload)
   messaging_events = data["entry"][0]["messaging"]
   for event in messaging_events:
     if "message" in event and "text" in event["message"]:
-      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-    else:
-      yield event["sender"]["id"], "I can't echo this"
+        return (event["sender"]["id"], event["message"]["text"].encode('unicode_escape'))
+    elif "postback" in event and "payload" in event["postback"]:
+        return (event["sender"]["id"], event["postback"]["payload"])
