d9d769abe1257c6766c728b2972172d1e003f36d
smit thakkar <smit@smits-MacBook-Air.local>
2016-08-13 17:35:58 +0530

    inital commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import requests
+import json
+import traceback
+import random
+from constants import *
+
+app = Flask(__name__)
+
+@app.route('/webhook')
+def webhook():
+    if request.method == 'GET':
+        if request.args.get('hub.verify_token') == VERIFY_TOKEN:
+            return request.args('hub.VERIFY_TOKEN')
+
+
+if __name__ == '__main__':
+    app.run()
