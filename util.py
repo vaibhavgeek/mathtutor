64aeb48477605c5dc9687ab0e52fa38f11eb94fe
Vaibhav Maheshwai <vaibhav.dkm@gmail.com>
2016-08-13 19:46:20 +0530

    added inter operations

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-from random import randint
+import random 
 
 def easy_addition():
 	a = randint(1,20)
@@ -9,37 +9,37 @@ def easy_addition():
 	option2 = answer + randint(3,5) 
 	option3 = answer + randint(5,7) 
 	option4 = answer + randint(8,11)
-	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) } 
+	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) , 'param1' : a , 'param2' : b } 
 	return dict 
 
 def easy_subtraction():
 	a = randint(1,20)
 	b = randint(1,20)
-	question = 'Add the numbers' + str(a) + ' and ' + str(b)
+	question = 'Subtract the numbers' + str(a) + ' and ' + str(b)
 	answer = a - b 
 	option1 = answer - randint(1,3) 
 	option2 = answer + randint(3,5) 
 	option3 = answer - randint(5,7) 
 	option4 = answer + randint(8,11)
-	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) } 
+	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) ,'param1' : a , 'param2' : b } 
 	return dict 
 
 def easy_multiply(): 
 	a = randint(1,20)
 	b = randint(1,20)
-	question = 'Add the numbers' + str(a) + ' and ' + str(b)
+	question = 'Multiply the numbers' + str(a) + ' and ' + str(b)
 	answer = a * b 
 	option1 = answer - randint(1,3) 
 	option2 = answer + randint(3,5) 
 	option3 = answer - randint(5,7) 
 	option4 = answer + randint(8,11)
-	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) } 
+	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) , 'param1' : a , 'param2' : b } 
 	return dict 
 
 def easy_divide(): 
 	a = randint(1,20)
 	b = randint(40,60)
-	question = 'Add the numbers' + str(a) + ' and ' + str(b)
+	question = 'Divide the numbers' + str(a) + ' and ' + str(b)
 	while(b%a == 0):
 		answer = b/a
 		a = a + 1
@@ -47,35 +47,36 @@ def easy_divide():
 	option2 = answer + randint(3,5) 
 	option3 = answer - randint(5,7) 
 	option4 = answer + randint(8,11)
-	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) } 
+	dict = { 'question': question , 'option1' : option1 , 'option2' : option2 , 'option3' : option3 , 'option4' : option4 , 'answer' : str(answer) , 'param1' : a , 'param2' : b } 
 	return dict 
 
 
 def medium_operation():
+  dic_add = easy_addition()
+  dic_sub = easy_subtraction
+  question = '' + questiondic_add['param1']  
+def advacned_operation():
 
 
-def advacned_operation
+def linear_easy():
 
 
-def linear_easy
+def linear_medium():
 
 
-def linear_medium 
+def linear_hard():
 
 
-def linear_hard 
+def quad_easy(): 
 
 
-def quad_easy 
+def quad_medium(): 
 
 
-def quad_medium 
+def quad_expert():
 
+def trigo_easy(): 
 
-def quad_expert 
+def trigo_medium(): 
 
-def trigo_easy 
-
-def trigo_medium 
-
-def trigo_hard  
+def trigo_hard():  
